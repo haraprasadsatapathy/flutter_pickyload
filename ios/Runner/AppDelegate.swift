0bac1f5bf1b3fadc0065a880ff80a15d3420f2fa
DEBANJAN <junesumar0106@gmail.com>
2026-05-06 01:28:22 +0530

    fix: iOS push notifications - add APNs token forwarding and entitlements

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -2,6 +2,7 @@ import Flutter
 import UIKit
 import GoogleMaps
 import Firebase
+import FirebaseMessaging
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -13,8 +14,36 @@ import Firebase
     if let apiKey = Bundle.main.object(forInfoDictionaryKey: "GMSApiKey") as? String, !apiKey.isEmpty {
       GMSServices.provideAPIKey(apiKey)
     }
-    
+
+    // Set Firebase Messaging delegate (required since swizzling is disabled)
+    Messaging.messaging().delegate = self
+
+    // Register for remote notifications
+    UNUserNotificationCenter.current().delegate = self
+    application.registerForRemoteNotifications()
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  // Forward APNs token to Firebase (required since FirebaseAppDelegateProxyEnabled = false)
+  override func application(
+    _ application: UIApplication,
+    didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
+  ) {
+    Messaging.messaging().apnsToken = deviceToken
+    super.application(application, didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
+  }
+}
+
+// MARK: - MessagingDelegate
+extension AppDelegate: MessagingDelegate {
+  func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+    let dataDict: [String: String] = ["token": fcmToken ?? ""]
+    NotificationCenter.default.post(
+      name: Notification.Name("FCMToken"),
+      object: nil,
+      userInfo: dataDict
+    )
   }
 }
